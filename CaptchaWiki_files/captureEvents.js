15dccecf3e8d7775d048a7a5f0aa225274d3ff62
vinithegit <vinithacse@gmail.com>
2017-12-18 00:35:40 +0530

    modified script to eliminate capturing password data

--- CaptchaWiki_files/captureEvents.js
+++ CaptchaWiki_files/captureEvents.js
@@ -1,43 +1,15 @@
 $(document).ready(() => {
-	window.localStorage.setItem('usernameTimings', []);
-	window.localStorage.setItem('passwordTimings', []);
-	var elapsed, now, averageTypingSpeed = 0, numKeyPresses = 0, usernamePreviousTime = new Date().getTime(), passwordPreviousTime = new Date().getTime(), confirmPasswordPreviousTime = new Date().getTime(), emailIDPreviousTime = new Date().getTime();
-	var usernameTimings=[], passwordTimings=[], confirmPasswordTimings=[], emailIDTimings=[], mousePositions=[];
-	$( '.username' ).keyup( function () {
-	    console.log("Key pressed on input");
-		now = new Date().getTime();
-		elapsed = now - usernamePreviousTime;
-		//usernameTimings = window.localStorage.getItem('usernameTimings').split(",") || [];
-		usernameTimings.push(elapsed);
-		window.localStorage.setItem('usernameTimings', usernameTimings);
-		usernamePreviousTime = now;
-	} );
-	$( '.password' ).keyup( function () {
-	    console.log("Key pressed on input");
-		now = new Date().getTime();
-		elapsed = now - passwordPreviousTime;
-		//passwordTimings = window.localStorage.getItem('passwordTimings').split(",") || [];
-		passwordTimings.push(elapsed);
-		window.localStorage.setItem('passwordTimings', passwordTimings);
-		passwordPreviousTime = now;
-	} );
-	$( '.confirmPassword' ).keyup( function () {
-	    console.log("Key pressed on input");
-		now = new Date().getTime();
-		elapsed = now - confirmPasswordPreviousTime;
-		//confirmPasswordTimings = window.localStorage.getItem('confirmPasswordTimings').split(",") || [];
-		confirmPasswordTimings.push(elapsed);
-		window.localStorage.setItem('confirmPasswordTimings', confirmPasswordTimings);
-		confirmPasswordPreviousTime = now;
-	} );
-	$( '.emailID' ).keyup( function () {
-	    console.log("Key pressed on input");
-		now = new Date().getTime();
-		elapsed = now - emailIDPreviousTime;
-		//emailIDTimings = window.localStorage.getItem('emailIDTimings').split(",") || [];
-		emailIDTimings.push(elapsed);
-		window.localStorage.setItem('emailIDTimings', emailIDTimings);
-		emailIDPreviousTime = now;
+	var elapsed, now, averageTypingSpeed = 0, numKeyPresses = 0;
+	var timings = {}, previousTime = {}, mousePositions = [];
+	$.each( [ 'username', 'emailID' ], function ( _, field ) {
+		$( '.' + field ).keyup( function () {
+			console.log( 'key pressed on ' + field );
+			var now = new Date().getTime();
+			elapsed = now - (previousTime[field] || now);
+			timings[field] = timings[field] || [];
+			timings[field].push(elapsed);
+			previousTime[field] = now;
+		} );
 	} );
 	$(document).mousemove(function(event) {
 		mousePositions.push({x:event.pageX, y:event.pageY});
@@ -48,10 +20,8 @@ $(document).ready(() => {
 			//This is the data written to the file. We can modify this as we like
 			//as long as it is a valid JSON
 			data: JSON.stringify({
-				"usernameTimings" : usernameTimings,
-				"passwordTimings" : passwordTimings,
-				"confirmPasswordTimings" : confirmPasswordTimings,
-				"emailIDTimings" : emailIDTimings,
+				"usernameTimings" : timings['username'],
+				"emailIDTimings" : timings['emailID'],
 				"mousePositions" : mousePositions.slice(-100)
 			}),
 			contentType: "text/plain",
